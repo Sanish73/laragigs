1db4c9a9e963b9774324ac9ab6cabeb6d15e84c2
Sanish Thapa <61194194+Sanish73@users.noreply.github.com>
2023-04-01 10:51:11 +0545

    asd

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,36 +2,11 @@
 
 namespace App\Models;
 
-use Psy\Readline\Hoa\Console;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Listing
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'loremDownload LDPlayer for Windows for free. Power Android 5.1 emulator. Enjoy the excellent mobile operating system Android from your computer with LDPlayer,...
-                Rating: 4.4 · ‎42 votes · ‎Free · ‎Windows · ‎Game'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'loremDownload LDPlayer for Windows for free. Power Android 5.1 emulator. Enjoy the excellent mobile operating system Android from your computer with LDPlayer,...
-                Rating: 4.4 · ‎42 votes · ‎Free · ‎Windows · ‎Game'
-            ]
-        ];
-    }
-
-    public static function find($id){
-        $listings = self::all();
-        
-    foreach($listings as $listing){
-    //   echo $listing;
-        if($listing['id'] ==$id){
-            return $listing;
-        }
-    }
-    }
-};
+    protected $table = 'listings';
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Listing::create([
+            'title' =>'Laravel Sinor Developer',
+            'tags' =>'laravel Javascript',
+            'company' =>'Amp camp',
+            'email' => 'emal.email@.com',
+            'location'=>'ktm',
+            'website' =>'https//www.ace.com',
+            'description' =>'Laravel makes connecting with databases and running queries extremely simple. The database configuration file is app/config/database. php . In this file you may define all of your database connections, as well as specify which connection should be used by default.'
+        ]);
+
+        Listing::create([
+            'title' =>'Laravel Junior Developer',
+            'tags' =>'laravel ',
+            'company' =>'ttump camp',
+            'location' => 'ckt',
+            'email' => 'emal.email@.com',
+            'website' =>'https//www.ace.com',
+            'description' =>'Laravel makes connecting with databases and running queries extremely simple. The database configuration file is app/config/database. php . In this file you may define all of your database connections, as well as specify which connection should be used by d'
+        ]);
     }
 }
